48e8e1c2e27083bd6ba2b796d779d1caaf4ac7c1
vinay143222 <vinaykumarhmvini@gmail.com>
2021-04-26 11:56:35 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+var output = [];
+var enter;
+for (i = 0; i < 5; i++) {
+    var enter = prompt("enter your 5 best friend names");
+    output.push(enter);
+}
+
+function test(output) {
+    var items = output.length;
+    var random = (Math.floor(Math.random() * items));
+    var respond = output[random];
+    return (respond + "" + " is your best friend");
+}
+test(output);
+document.getElementById("friend").innerHTML = test(output);
